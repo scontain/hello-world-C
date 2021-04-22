f973b0c33bb152a916d88b830086bdd727efde66
vasyl_lukashuk <vas77794@gmail.com>
2021-04-22 17:25:55 +0200

    Update to 5.3.2

--- run.sh
+++ run.sh
@@ -9,6 +9,6 @@ echo "Run sconified image..." && \
 docker run -it --rm --network=host $MOUNT_SGXDEVICE \
 -eSCONE_VERSION=1 \
 -eSCONE_MODE=hw \
--eSCONE_LOG="7" \
+-eSCONE_LOG="WARNINGS" \
 -ePATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin" \
 hello-world-c-scone /app/hello-world
